8ac22fb89a5698cd7b85058c0dc0a6470c3bd17d
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2020-05-31 22:32:50 -0400

    Add convert lspath to rust

---
+++ setup/tools/lspath/src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::vec::Vec;
+use ansi_term::Colour::Red;
+use ansi_term::Colour::Yellow;
+use ansi_term::Colour::Green;
+use ansi_term::Colour::Blue;
+use ansi_term::Colour::Purple;
+use atty::Stream;
+
+fn main() {
+    let path: String = env::var("PATH")
+        .expect("Expected to find path env variable");
+    let entries: Vec<&str> = path.split(":").collect();
+    
+    for entry in entries {
+        let breakdown: Vec<&str> = entry.split("/").collect();
+        if !atty::is(Stream::Stdout) {
+            println!("{}", entry);
+            continue;
+        }
+
+        let mut output: Vec<String> = Vec::new();
+        for (i, item) in breakdown.iter().enumerate() {
+            let offset = i % 5;
+            if offset == 0 {
+                output.push(format!("{}", Red.paint(*item)));
+            } if offset == 1 {
+                output.push(format!("{}", Yellow.paint(*item)));
+            } if offset == 2 {
+                output.push(format!("{}", Green.paint(*item)));
+            } if offset == 3 {
+                output.push(format!("{}", Blue.paint(*item)));
+            } if offset == 4 {
+                output.push(format!("{}", Purple.paint(*item)));
+            }
+        }
+
+        println!("{}", output.join("/"));
+    }
+}
